cfd9240188891415a89aaee678c98e48dfe501c7
rodrigoarcadio1@yahoo.es <rodrigoarcadio1@yahoo.es>
2024-09-01 17:06:22 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { LoginPage } from "./components/LoginPage";
+
+function App() {
+  return (
+    <div>
+      <h1>Zustand React</h1>
+
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;

814994b5168746e52c6ab934e6a8665f2ade6301
rodrigoarcadio1@yahoo.es <rodrigoarcadio1@yahoo.es>
2024-09-01 17:27:27 +0200

    update

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,13 +1,30 @@
+import { useEffect } from "react";
 import { useAuthStore } from "../store/auth.store";
 
 export const LoginPage = () => {
   const authStatus = useAuthStore((state) => state.status);
 
+  const login = useAuthStore((state) => state.login);
+
+  const logout = useAuthStore((state) => state.logout);
+
+  const user = useAuthStore((state) => state.user);
+
+  useEffect(() => {
+    setTimeout(() => {
+      logout();
+    }, 1500);
+  });
+
   return (
     <>
       <h3> LoginPage </h3>
 
-      {authStatus}
+      {authStatus === "authenticated" ? (
+        <div> {JSON.stringify(user, null, 2)}</div>
+      ) : (
+        <div>No authenticated</div>
+      )}
     </>
   );
 };
